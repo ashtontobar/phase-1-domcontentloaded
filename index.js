476813bb1237a34c9c59812a459cd0c4ac6bbc12
Ashton Tobar <+>
2022-06-05 14:22:39 -0500

    commit tree e51841ef14989f2b563b9dffa4b7cea0955582f6

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+// document.addEventListener("DOMContentLoaded", function(){
+//     let txt = document.getElementById("text")
+//     txt.textContent = "This is really cool!"
+// })
+
+document.addEventListener("DOMContentLoaded", () => document.getElementById("text").textContent = "This is really cool!")
+
+
